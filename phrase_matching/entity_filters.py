489d2e116382eff39ff1e0c2cdddd3c7a3dac3b6
yasufumi <yasufumi.taniguchi@gmail.com>
2021-08-25 14:16:11 +0900

    Add docstring to entity_filters.py

--- phrase_matching/entity_filters.py
+++ phrase_matching/entity_filters.py
@@ -5,7 +5,16 @@ from .core import Entity
 
 
 def overlap(a: Entity, b: Entity) -> bool:
-    return a.start_offset <= b.end_offset and a.start_offset <= b.end_offset
+    """Checks if one entity overlaps with another entity.
+
+    Args:
+      a: One entity.
+      b: Another entity.
+
+    Returns:
+      True if there is an overlap, False otherwise.
+    """
+    return a.start_offset <= b.end_offset and b.start_offset <= a.end_offset
 
 
 class EntityFilter:
@@ -16,10 +25,18 @@ class EntityFilter:
 
 class LongestMatchFilter:
     def __call__(self, entities: List[Entity]) -> List[Entity]:
-        # TODO: not efficient
+        """Removes overlapping entities and leaves the longest entity.
+
+        Args:
+          entities: A list of entities.
+
+        Returns:
+          A list of entities without any overlaps.
+        """
         entities = sorted(entities, key=len, reverse=True)
         filtered: List[Entity] = []
         for cur in entities:
             if all(not overlap(cur, prev) for prev in filtered):
                 filtered.append(cur)
+        filtered.sort(key=lambda entity: entity.start_offset)
         return filtered
