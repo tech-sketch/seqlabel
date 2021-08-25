1bb47a08d2f6d8b57098f5240c16487674506add
yasufumi <yasufumi.taniguchi@gmail.com>
2021-08-25 13:30:46 +0900

    Remove old codes

--- phrase_matching/core.py
+++ phrase_matching/core.py
@@ -1,40 +1,8 @@
 from abc import abstractmethod
 from dataclasses import dataclass
-from typing import Callable, Iterator, List, Tuple, Union
+from typing import Iterator, List, Tuple, Union
 
-from intervaltree import IntervalTree
 from tokenizations import get_alignments
-
-from ahocorasick import Automaton
-from phrase_matching import utils
-
-
-def labeling_from_text(text: str, automaton: Automaton, tokenizer: Callable[[str], List[str]]) -> List[str]:
-    """Attaches labels to a given text."""
-    tokens = tokenizer(text)
-    return labeling_from_tokens(tokens, text, automaton)
-
-
-def labeling_from_tokens(tokens: List[str], text: str, automaton: Automaton) -> List[str]:
-    """Attaches labels to a given text."""
-    mapping = utils.get_offset_mapping(text, tokens)
-    tree = IntervalTree()
-
-    for end_char, (label, length) in automaton.iter(text):
-        start_char = end_char - length + 1
-
-        valid, start_token, end_token = utils.convert_to_token_span(mapping, start_char, end_char)
-
-        if valid:
-            end_token += 1
-            spans = tree.overlap(start_token, end_token)
-            if not spans:
-                tree.addi(start_token, end_token, label)
-            elif all(end_token - start_token > span.end - span.begin for span in spans):
-                tree.remove_overlap(start_token, end_token)
-                tree.addi(start_token, end_token, label)
-
-    return utils.convert_to_tags(tree, len(tokens))
 
 
 class StringSequence:
@@ -129,10 +97,3 @@ class Entity:
 
     def __len__(self) -> int:
         return self.end_offset - self.start_offset + 1
-
-
-@dataclass
-class Span:
-    start_offset: int
-    end_offset: int
-    label: str
