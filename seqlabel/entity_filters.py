12852a1a6caaff707d7b70e20c825f3c72eca032
Taniguchi Yasufumi <yasufumi.taniguchi@gmail.com>
2021-09-02 15:30:11 +0900

    Add a new entity filter (#5)
    * Add MaximizedMatchFilter
    
    * Add test cases for MaximizedMatchFilter
    
    * Add some comments
    
    * Bump version in pyproject.toml

--- seqlabel/entity_filters.py
+++ seqlabel/entity_filters.py
@@ -18,12 +18,16 @@ def overlap(a: Entity, b: Entity) -> bool:
 
 
 class EntityFilter:
+    """Base class of all entity filters."""
+
     @abstractmethod
     def __call__(self, entities: List[Entity]) -> List[Entity]:
         pass
 
 
 class LongestMatchFilter:
+    """Entity filter prioritizes entity length."""
+
     def __call__(self, entities: List[Entity]) -> List[Entity]:
         """Removes overlapping entities and leaves the longest entity.
 
@@ -39,4 +43,26 @@ class LongestMatchFilter:
             if all(not overlap(cur, prev) for prev in filtered):
                 filtered.append(cur)
         filtered.sort(key=lambda entity: entity.start_offset)
+        return filtered
+
+
+class MaximizedMatchFilter:
+    """Entity filter prioritizes the number of entities."""
+
+    def __call__(self, entities: List[Entity]) -> List[Entity]:
+        """Removes overlapping entities and leaves as many entities as possible.
+
+        Args:
+          entities: A list of entities.
+
+        Returns:
+          A list of entities without any overlaps.
+        """
+        entities = sorted(entities, key=lambda entity: entity.end_offset)
+        filtered: List[Entity] = []
+        for entity in entities:
+            if not filtered:
+                filtered.append(entity)
+            elif not overlap(filtered[-1], entity):
+                filtered.append(entity)
         return filtered
