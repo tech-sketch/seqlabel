6189d849a55980d99df436a88ce7a1a5fcb74671
Taniguchi Yasufumi <yasufumi.taniguchi@gmail.com>
2021-10-04 12:02:41 +0900

    Add a serializer for the IOBES format (#12)
    * Add IOBESSerializer
    
    * Add a test case for IOBESSerializer
    
    * Bump version

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -4,7 +4,7 @@ from typing import List
 import pytest
 
 from seqlabel.core import Entity, StringSequence
-from seqlabel.serializers import BILOUSerializer, IOB2Serializer, JSONLSerializer
+from seqlabel.serializers import BILOUSerializer, IOB2Serializer, IOBESSerializer, JSONLSerializer
 
 
 @pytest.mark.parametrize(
@@ -32,6 +32,20 @@ def test_jsonl_serializer_save(text_ja: StringSequence, entities: List[Entity], 
 )
 def test_iob2_serializer_save(text_ja: StringSequence, entities: List[Entity], expected: str) -> None:
     serializer = IOB2Serializer()
+    assert serializer.save(text_ja, entities) == expected
+
+
+@pytest.mark.parametrize(
+    "entities,expected",
+    [
+        (
+            [Entity(6, 8, "LOC")],
+            "日\tO\n本\tO\nの\tO\n首\tO\n都\tO\nは\tO\n東\tB-LOC\n京\tI-LOC\n都\tE-LOC\nで\tO\nす\tO\n。\tO",
+        )
+    ],
+)
+def test_iobes_serializer_save(text_ja: StringSequence, entities: List[Entity], expected: str) -> None:
+    serializer = IOBESSerializer()
     assert serializer.save(text_ja, entities) == expected
 
 
